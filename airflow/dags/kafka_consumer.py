dac8bb81273430fe9ad1a497d6658517fa53c5a5
nat99up <nat99up@gmail.com>
2024-03-18 22:42:35 +0800

    feat: handle insertion

--- airflow/dags/kafka_consumer.py
+++ airflow/dags/kafka_consumer.py
@@ -2,6 +2,20 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 import datetime
 import json
+import psycopg2
+
+def insert_event_data(event_data):
+  connection = psycopg2.connect(dbname="airflow", user="airflow", password="airflow", host="postgres", port=5432)
+  cursor = connection.cursor()
+
+  insert_statement = """
+    INSERT INTO ad_events (action, location)
+    VALUES (%s, %s)
+  """
+  cursor.execute(insert_statement, (event_data["action"], event_data["location"]))
+  connection.commit()
+  cursor.close()
+  connection.close()
 
 default_args = {
     'owner': 'airflow',
@@ -15,10 +29,6 @@ dag = DAG(
     default_args=default_args,
     max_active_runs=1
 )
-
-def print_event(topic, event):
-    print(f"topic: {topic}")
-    print(f"event: {event}")
 
 def listen_kafka():
     from kafka import KafkaConsumer
@@ -34,15 +44,20 @@ def listen_kafka():
     for message in consumer:
         topic = message.topic
         event = message.value.decode('utf-8')
-        print_event(topic, event)
-        event_dict = json.loads(event)
 
-        if topic == 'ad_event':
-            print(f"Got ad_event: {event_dict}")
-        elif topic == 'bot_event':
-            print(f"Got bot_event: {event_dict}")
-        else:
-            print(f"Unsupported event_type: {event_dict['event_type']}")
+        try:
+            event_data = json.loads(event)
+            if topic == 'ad_event':
+                print(f"Got ad_event: {event_data}")
+                insert_event_data(event_data)
+                print("Insertion Successful!")
+            elif topic == 'bot_event':
+                print(f"Got bot_event: {event_data}")
+            else:
+                print(f"Unsupported topic: {topic}")
+        except:
+            print("Something wrong")
+
 
 kafka_consumer_task = PythonOperator(
     task_id='kafka_consumer',
